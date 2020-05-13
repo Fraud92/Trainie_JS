a19b3223c051b1fca4420e02565689efe772f01f
Alexander Mill <me@okatiev.ru>
2020-05-13 19:28:24 +0300

    My frst JS commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let incr = 20,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && !isClose);
