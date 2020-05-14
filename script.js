18ce4b2f52437e1e3ef20087e5f401f6f8ebf9ec
Alexander Mill <me@okatiev.ru>
2020-05-14 17:46:42 +0300

    For, while, do-while with answer popups

--- script.js
+++ script.js
@@ -10,12 +10,67 @@ const personalMoviesDB = {
     privat: false
 };
 
-const a = prompt('Какой фильм вы недавно смотрели?', ''),
-      b = prompt('И как он вам?', ''),
-      c = prompt('Какой фильм ещё смотрели недвано?', ''),
-      d = prompt('И как он вам?', '');
+// 1
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Какой фильм вы недавно смотрели?', ''),
+//           b = prompt('И как он вам?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMoviesDB.movies[a] = b;
+//         console.log('Done!');
+//     } else {
+//         console.log('Error!');
+//         i--;
+//     }
+
+    
+// }
+
+
+let i = 0;
+
+// while (i < 2) {
+
+//     const a = prompt('Какой фильм вы недавно смотрели?', ''),
+//           b = prompt('И как он вам?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMoviesDB.movies[a] = b;
+//         console.log('Done!');
+//     } else {
+//         console.log('Error!');
+//         i--;
+//     }
+
+//     i++;
+// }
+
+
+do {
+    const a = prompt('Какой фильм вы недавно смотрели?', ''),
+            b = prompt('И как он вам?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMoviesDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error!');
+        i--;
+    }
+
+    i++;
+} while (i < 2);
+
+
+if (personalMoviesDB.count < 10) {
+    console.log('Мало фильмов поглядел...');
+} else if(personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log('Норм так фильмов поглядел...');
+} else if (personalMoviesDB.count >= 30) {
+    console.log('Киноманищееее...');
+} else {
+    console.log('SHIT HAPPENS...');
+}
 
 console.log(personalMoviesDB);
