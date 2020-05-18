6d80788d9adcb69c2b820669d98fe6638a108990
Alexander Mill <me@okatiev.ru>
2020-05-18 12:11:08 +0300

    Деструктуризация данных объекта

--- script.js
+++ script.js
@@ -110,16 +110,59 @@
 
 //console.log(personalMoviesDB);
 
-function done() {
-    console.log("Я стал Яваскриптизёром через 3 секунды таймаута!!!!");
-}
+// function done() {
+//     console.log("Я стал Яваскриптизёром через 3 секунды таймаута!!!!");
+// }
 
-function learnJS(lang, callback) {
-    console.log(`Я учу ${lang}!`);
+// function learnJS(lang, callback) {
+//     console.log(`Я учу ${lang}!`);
     
-    setTimeout(function() {
-        callback();
-    }, 3000);
-}
+//     setTimeout(function() {
+//         callback();
+//     }, 3000);
+// }
 
-learnJS('JavaScript', done);
+// learnJS('JavaScript', done);
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function() {
+        console.log("Test manual method...");
+    }
+};
+
+options.makeTest();
+
+const {border, background} = options.colors;
+
+console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.colors.border);
+
+// // delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
